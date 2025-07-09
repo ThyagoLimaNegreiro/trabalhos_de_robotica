125574964ecec1d601ff034cfc92c3a43846dd13
ThyagoLimaNegreiro <thyagolimanegreiro2@gmail.com>
2025-07-09 15:20:22 -0300

    Create 6questão.cpp

---
+++ 6questão.cpp
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int soma(int x, int y){
+    int soma = x + y;
+    return soma;
+}
+
+int main()
+{
+   int res = soma(10, 16);
+   cout << res << endl;
+}
