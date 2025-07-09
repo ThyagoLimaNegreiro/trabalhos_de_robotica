b5c69fccdc75f1648d11683c0ea58b9d267dab6e
ThyagoLimaNegreiro <thyagolimanegreiro2@gmail.com>
2025-07-09 14:30:45 -0300

    Create 1questão.cpp

---
+++ 1questão.cpp
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+   int n1 = 10;
+   float n2 = 3.1;
+   double n3 = 3.14;
+   char letra = 'l';
+   std:: string nome = "Luiz";
+   cout << n1 << "\n" << n2 << "\n" << n3 << "\n" << letra << "\n" << nome << endl;
+}
